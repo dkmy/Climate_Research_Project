272baa8c5d76424ba064341aff701e1f21363297
David Yang <yangd5153@gmail.com>
2016-02-14 20:27:43 -0500

    ok w/e

---
+++ model_error_Map.py
@@ -0,0 +1,141 @@
+__author__ = 'DavidKMYang'
+#https://plot.ly/python/getting-started
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import scipy
+import os
+import scipy.io
+import numpy as np
+import plotly.plotly as py
+import glob
+import pandas as pd
+from numpy import genfromtxt
+
+
+def hist_plot(header, yearStart, yearEnd, monthBool, monthStart, monthEnd, customBool):
+
+    if header == "tmax":
+        path_wbgt = '/Users/DavidKMYang/ClimateResearch/WBGT/ncep_tasmax_nh/'
+    elif customBool == 0:
+        path_wbgt = '/Users/DavidKMYang/ClimateResearch/WBGT/gfdl_tasmax_nh/'
+    else:
+        path_wbgt = '/Users/DavidKMYang/ClimateResearch/WBGT/corrected_gfdl_tasmax_nh/'
+
+
+    os.chdir(path_wbgt)
+
+    file_names_wbgt = []
+
+    for n in range(yearEnd - yearStart + 1):
+        if (int(monthBool) == 0):
+            file_names_wbgt = glob.glob(header + "_" + str(yearStart + n) + "*.mat")
+
+        else:
+            for i in range(monthEnd - monthStart + 1):
+                if monthStart + i < 10:
+                    file_names_temp = glob.glob(header + "_" + str(yearStart + n) + "_" + "0" + str(monthStart + i) + "*.mat")
+                else:
+                    file_names_temp = glob.glob(header + "_" + str(yearStart + n) + "_" + str(monthStart + 1) + "*.mat")
+                file_names_wbgt.extend(file_names_temp)
+
+    totalDays = 0
+    actualTotalDays = 0
+    Val2D = []
+
+    for n in range(len(file_names_wbgt)):
+        if (customBool == 0):
+            tempData_wbgt = scipy.io.loadmat(path_wbgt + file_names_wbgt[n])
+            tempData_wbgt = tempData_wbgt[file_names_wbgt[n][:-4]][0]
+            tempData_Val_wbgt = tempData_wbgt[2]
+        else:
+            tempData_wbgt = scipy.io.loadmat(path_wbgt + file_names_wbgt[n])
+
+            # tempData_Lat_temp = tempData_wbgt[file_names_wbgt[i][:-4]+"_Lat"]
+            # tempData_Long_temp = tempData_wbgt[file_names_wbgt[i][:-4] + "_Long"]
+            tempData_Val_wbgt = tempData_wbgt[file_names_wbgt[n][:-4] + "_Val"]
+
+        # print (len(tempData_Val_wbgt))
+        # break
+        actualTotalDays += len(tempData_Val_wbgt[0][0])
+
+        if n == 0:
+            for i in range(len(tempData_Val_wbgt)):
+                tempLatList = []
+                for k in range(len(tempData_Val_wbgt[0])):
+                    tempLong = 0
+                    for j in range(len(tempData_Val_wbgt[0][0])):
+                        tempLong += (tempData_Val_wbgt[i][k][j])
+                        totalDays += 1
+                    tempLatList.append(tempLong)
+                Val2D.append(tempLatList)
+            break
+
+        for i in range(len(tempData_Val_wbgt)):
+            for k in range(len(tempData_Val_wbgt[0])):
+                tempLong = 0
+                for j in range(len(tempData_Val_wbgt[0][0])):
+                    tempLong += (tempData_Val_wbgt[i][k][j])
+                    totalDays += 1
+                Val2D[i][k] += tempLong
+
+    for i in range(len(Val2D)):
+        for k in range(len(Val2D[0])):
+            Val2D[i][k] /= actualTotalDays
+
+    Val2D = np.array(Val2D)
+    return Val2D
+
+def Plot(array):
+    path_wbgt = '/Users/DavidKMYang/ClimateResearch/WBGT/gfdl_tasmax_nh/'
+
+    tempData_wbgt = scipy.io.loadmat(path_wbgt + 'tasmax_2005_07_01.mat')
+    tempData_wbgt = tempData_wbgt['tasmax_2005_07_01'][0]
+
+    flatTLat = np.array(tempData_wbgt[0])
+    flatTLon = np.array(tempData_wbgt[1])
+    flatTData = np.array(array)
+
+    # m = Basemap(width=10000000,height=10000000,
+    #             resolution='l',projection='kav7',
+    #             lat_ts = 10, lat_0=30, lon_0 = 30)
+    m = Basemap(projection = 'kav7', lon_0 = 0, resolution = 'l')
+
+    lon, lat = np.meshgrid(flatTLon[0,:], flatTLat[:,0])
+    x, y = m(lon,lat)
+    cs = m.pcolor(x,y,np.squeeze(flatTData), vmin=-15, vmax=15)
+    m.drawmapboundary(fill_color='0.3')
+    # Add Grid Lines
+    m.drawparallels(np.arange(-80., 81., 10.), labels=[1,0,0,0], fontsize=10)
+    m.drawmeridians(np.arange(-180., 181., 10.), labels=[0,0,0,1], fontsize=10)
+
+    # Add Coastlines, States, and Country Boundaries
+    m.drawcoastlines()
+    m.drawstates()
+    m.drawcountries()
+
+    # Add Colorbar
+    cbar = m.colorbar(cs, location='bottom', pad="10%")
+
+    # Add Title
+    plt.title('n')
+
+    plt.show()
+
+model_Data = hist_plot("tasmax", 1994, 2005, 0, 0, 0, 0)
+historical_Data = hist_plot("tmax", 1994, 2005, 0, 0, 0, 0)
+
+# print (historical_Data)
+differences = []
+for i in range(len(model_Data)):
+    tempArray = []
+    for k in range(len(model_Data[0])):
+        tempArray.append(model_Data[i][k] - historical_Data[i][k]) #subtract model from historical , model - historical
+    differences.append(tempArray)
+
+print (len(differences[0]))
+Plot(differences)
+
+
+
+
